f3c94bdcf8a4ddd7c06022513f990bdb859d7c85
Nathanael Yang <ny83427@gmail.com>
2019-01-03 02:14:34 -0800

    adjust directory

--- copy-template.bat
+++ copy-template.bat
@@ -1,10 +1,10 @@
 @echo off
 for %%G in (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18) do (
-    if not exist slides\chapter%%G (
-        mkdir slides\chapter%%G
+    if exist chapter%%G (
+        xcopy chapter%%G\* ..\blog\public\tutorial\chapter%%G\ /d /y /e
     )
-    if not exist slides\chapter%%G\Readme.md (
-        echo ## Under Construction>> slides\chapter%%G\Readme.md
-    )
-    copy slides\template.html slides\chapter%%G\index.html
 )
+xcopy node_modules\* ..\blog\public\tutorial\node_modules\ /d /y /e
+xcopy image\* ..\blog\public\tutorial\image\ /d /y /e
+copy favicon.ico ..\blog\public\tutorial\favicon.ico
+copy index.html ..\blog\public\tutorial\index.html
